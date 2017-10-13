feaa8995e2954fd4f4a395815989d47bdf4a67a5
aricalves <aric.alves2012@gmail.com>
2017-10-12 21:03:34 -0700

    pass two tests for tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,40 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
+  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, node) {
+  // if node exists 
+  //   set node to equal node
+  // else node equals this
+  // if this value equals target
+  //   return true
+  // else if current node has children
+  //   for each child
+  //     return call contains on each child
+  // else return false
+  node = node || this;
+  if ( this.value === target ) {
+    return true;
+  } else if ( node.children ) {
+    _.every(function (child) {
+      return this.contains(target, child);
+    });
+  } else {
+    return false;
+  }
+  
 };
 
 
