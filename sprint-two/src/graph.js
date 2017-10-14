6ba94eacfd53ed312e48b61d765cb148ef1cbc9f
aricalves <aric.alves2012@gmail.com>
2017-10-13 17:17:00 -0700

    Complete graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,109 +1,89 @@
-
-
 // Instantiate a new graph
-var Graph = function() {
-
-  this.nodes = [];
-
+var Graph = function(value) {
 };
-
-var Node = function (value) {
-  this.value = value;
-  this.edge = [];
-};
-
-// Add a node to the graph, passing in the node's value.
+// Add a node to the graph
 Graph.prototype.addNode = function(value) {
-  this.nodes.push(new Node(value));
-  
+  this[value] = [];
+  // console.log(this);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(target) {
-  // loop through nodes array
-  //   if any node contains value
-  //      return true
-  //   else return false
-  for ( var i = 0; i < this.nodes.length; i++ ) {
-    if ( this.nodes[i].value === target ) {
-      return true;
-    }   
-  }
-  return false;
+  target = JSON.stringify(target);
+  var result = false;
+  this.forEachNode(function(node) {
+    if ( node === target ) {
+      result = true;
+    }
+  });
+  return result;  
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(value) {
-  // loop through nodes array
-  //  if any nodes contains value 
-  //    splice out the node  from the array
-  for ( var i = 0; i < this.nodes.length; i++ ) {
-    if ( this.nodes[i].value === value ) {
-      this.nodes.splice(i, 1);
-    }   
-  }
+  var valueEdges = this[value];
+
+  //  remove value references from edges
+  for (let i = 0; i < valueEdges.length; i++) {
+    var edge = valueEdges[i];
+    var indexOfRemoveNode = this[edge].indexOf(value);
+    this[edge].splice(indexOfRemoveNode, 1);  
+  }    
+  delete this[value];  
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var edges = this[fromNode];
+  return edges.indexOf( toNode) >= 0;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  // 
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-};
-
-// Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
-  // loop through each node in nodes
-  //   pass the cb on each node
-  for (var i = 0; i < this.nodes.length; i++) {
-    cb(this.nodes[i]);
+  // for edges in fromNode
+  //  if edges hasIndexOf toNode
+  //    remove toNode (splice edges at index)
+  var fromIndex = this[fromNode].indexOf(toNode);
+  var toIndex = this[toNode].indexOf(fromNode);
+  if ( fromIndex >= 0 ) {
+    this[fromNode].splice(toIndex, 1);
+  }
+  if ( toIndex >= 0 ) {
+    this[toNode].splice(fromIndex, 1);
   }
 };
 
-// search through the nodes and return the target
-Graph.prototype.findNode = function (target) {
-  //  for each node
-  //     if target === node value
-  //     return node
-
-  this.forEachNode(function(node) {
-    if ( node.value === target ) {
-      return this.node;
-    }
-  });
-
+// Pass in a callback which will be executed on each node of the graph.
+Graph.prototype.forEachNode = function(cb, nodes) {
+  // loop through each node in nodes
+  //   pass the cb on each node
+  nodes = nodes || Object.keys(this);
+  for (var i = 0; i < nodes.length; i++) {
+    cb(nodes[i], i, nodes);
+  }
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
 
-var test = new Graph();
-test.addNode(33);
-var output = test.nodes;
-//console.log(output);
-test.addNode(77);
-var output = test.nodes;
-//console.log(output);
 
-var orange = function (node) {
-  node.color = 'orange';
-};
+// Complexity: What is the time complexity of the above functions?
 
-test.forEachNode(orange);
+//"addNode" -  O(1)
+//"contains"  -  O(n)
+//"removeNode" - O(n2)
+//"addEdge" - O(1)
+//"hasEdge" - O(n)
+//"removeEdge" - O(n)
+//"forEachNode" - O(n)
 
-console.log(output);
 
-var find = test.findNode(33);
 
-console.log(find);
  
  
 
